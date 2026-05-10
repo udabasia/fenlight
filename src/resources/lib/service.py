93dd02d971616d469f8dc2637ea24a2fcdba0b13
udabasia <umesh.dabasia@gmail.com>
2026-05-10 13:11:58 +0100

    a

--- src/resources/lib/service.py
+++ src/resources/lib/service.py
@@ -82,9 +82,7 @@ class OnUpdateChanges:
 			clear_all_trakt_cache_data(silent=True, refresh=False)
 			show_dialog = True
 		if show_dialog:
-			set_setting('fenlight.trakt.client', "6d77dc23a83a021cfa2285d6f91ea70a691b2857cd25ca21df46b18786c18eea")
-			set_setting('trakt.secret', "827c62f2e069298310b6935991b3603205d34ca052db160361d475517c937ca7")
-			set_setting('tmdb_api', "e325eaa35bd506111b6cb61eebbc803e")
+			return
 
 class CustomWindowsPrepare:
 	def run(self):
