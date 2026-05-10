367fe4807d135da6d5960dd04b56e7548a930124
udabasia <umesh.dabasia@gmail.com>
2026-05-10 18:24:25 +0100

    a

--- package.py
+++ package.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import shutil
+import hashlib
 import os
 
 tree = ET.parse('src/addon.xml')
@@ -20,3 +21,12 @@ shutil.copytree(f"src", f"./__temp__/{addon_id}", dirs_exist_ok=True)
 shutil.make_archive(output_filename, 'zip', "./__temp__")
 
 shutil.rmtree('./__temp__')
+
+
+with open("addons.xml", "rb") as f:
+    data = f.read()
+
+md5 = hashlib.md5(data).hexdigest()
+
+with open("addons.xml.md5", "w") as f:
+    f.write(md5)
